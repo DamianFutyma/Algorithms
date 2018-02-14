cf811b265c82abe2fad0c30cbcd80039ce4b0f74
DamianFutyma <futymadamian@wp.pl>
2018-02-14 17:40:50 +0100

    First exercise from Codewars with tests

---
+++ src/main/java/SortOnlyOdds.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class SortOnlyOdds {
+
+    SortOnlyOdds(int[]oddsAndEvenNumber){
+
+    }
+
+    int[] sortIgnoringOdds(int[] givenArray) {
+        int[] oddNumbersArray = Arrays.stream(givenArray).filter(value -> value % 2 != 0).sorted().toArray();
+        for (int i = 0; i < givenArray.length; i++) {
+            if (givenArray[i] % 2 != 0) {
+                givenArray[i] = -1;
+            }
+        }
+        for (int integer : oddNumbersArray) {
+            for (int j = 0; j < givenArray.length; j++) {
+                if (givenArray[j] == -1) {
+                    givenArray[j] = integer;
+                    break;
+                }
+            }
+        }
+        return givenArray;
+    }
+}
